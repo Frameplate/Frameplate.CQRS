74691a0533b810549793c547c030760217d34300
eskat0n <muyou.prj@gmail.com>
2015-05-06 13:54:48 +0500

    added basic criterion types

---
+++ sources/Frameplate.CQRS/Criteria/IdListCriterion.cs
@@ -0,0 +1,12 @@
+﻿namespace Frameplate.CQRS.Criteria
+{
+    public class IdListCriterion<TIdType> : ICriterion
+    {
+        public IdListCriterion(params TIdType[] idList)
+        {
+            IdList = idList;
+        }
+
+        public TIdType[] IdList { get; }
+    }
+}
